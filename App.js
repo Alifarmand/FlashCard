9b29e19dbbe9704a6ecb72029a325641188b1925
Ali Farmand <lolinated@gmail.com>
2018-03-26 21:37:24 +0200

    Setting up some basics for Appjs

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import styled from 'styled-components'
+
+const store = createStore(reducer, applyMiddleware(thunk))
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <Wrapper>
+          <Text>Testing if this works!</Text>
+          <Text>YES IT DOES!</Text>
+        </Wrapper>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Wrapper = styled.View`
+  flex: 1;
+  justifyContent: center;
+  background-color: #EBEBEB;
+  align-items: center;
+`
